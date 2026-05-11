2ef4deac9a7c9820b092a6e4869b1622a384fdb6
KarenK1235 <karen@reoptionsllc.com>
2026-05-11 11:36:12 -0700

    Delete components/ui/button.tsx

--- components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Button({ className = "", children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button className={className} {...props}>
-      {children}
-    </button>
-  );
-}
