c9bf015a9092305cb688271203a8e58d6452b9e4
KarenK1235 <karen@reoptionsllc.com>
2026-05-11 11:34:13 -0700

    Create textarea.tsx

---
+++ components/ui/components/ui/components/ui/textarea.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Textarea({ className = "", ...props }: React.TextareaHTMLAttributes<HTMLTextAreaElement>) {
+  return <textarea className={className} {...props} />;
+}
