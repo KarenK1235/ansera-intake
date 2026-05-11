e0663a96ab3c9b35ea51f2587df1bfe3db9fbbab
KarenK1235 <karen@reoptionsllc.com>
2026-05-11 11:37:08 -0700

    Delete components/ui/input.tsx

--- components/ui/input.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Input({ className = "", ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={className} {...props} />;
-}
